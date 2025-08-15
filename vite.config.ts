b9c70a65fc1eb5a0e893d9e892cfa8342961b219
André <marketing@paviloche.con.br>
2025-08-15 14:09:39 -0300

    Updated vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  root: path.resolve(__dirname, 'frontend'),
   build: {
-    outDir: 'build'
-  }
+    outDir: path.resolve(__dirname, 'frontend/build'),
+    emptyOutDir: true,
+  },
+  server: {
+    port: 3000,
+    hmr: {
+      host: 'localhost',
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'frontend/src'),
+    },
+  },
 });
